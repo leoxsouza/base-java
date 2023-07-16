a0d1c340bf0b20cc7e296f84f58e9fc600177206
Leonardo Oliveira <leoxsouza286@gmail.com>
2023-07-16 12:20:17 -0300

    refactor: Alterando pacotes e nomes

---
+++ java-8/examples-features/src/main/java/lambda/expressions/FiguraExemplo.java
@@ -0,0 +1,25 @@
+package lambda.expressions;
+
+interface Figura {
+    void desenha();
+}
+
+
+public class FiguraExemplo {
+
+    public static void main(String[] args) {
+
+        // Exemplo sem usar lambda
+        Figura figura1 = new Figura() {
+            @Override
+            public void desenha() {
+                System.out.println("Desenha figura 1");
+            }
+        };
+        figura1.desenha();
+
+        Figura figura2 = () -> System.out.println("Desenha figura 2");
+        figura2.desenha();
+
+    }
+}
